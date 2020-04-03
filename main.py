b3ec8fdb3437c216dae5a3e1c3d2d0f6d61b1b2a
Constantine M. Lapkin <constlapkin@gmail.com>
2020-04-03 17:51:14 +1000

    added import and export in csv file
    Signed-off-by: Constantine M. Lapkin <constlapkin@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sqlite3
 from datetime import datetime
+import csv
+import os.path
 
 
 ELECTRICITY_BILL = 4.51
@@ -72,13 +74,50 @@ def init(conn, cursor):
     conn.commit()
 
 
+def export_csv(data, name):
+    with open("{0}.csv".format(name), "w", newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, dialect="excel")
+        for el in data:
+            el = list(el)
+            csv_writer.writerow(el)
+    print("{0}.csv file".format(name))
+
+
+def import_csv(conn, cursor, name, type):
+    with open("{0}".format(name), "r", newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, dialect="excel")
+        for row in csv_reader:
+            d = list()
+            if len(row) == 3:
+                d.append(row[1])
+                d.append(row[2])
+            elif len(row) == 2:
+                d.expand(row)
+            elif len(row) > 3 or len(row) < 2:
+                print("error import")
+                break
+            if type == "cold":
+                db_insert_cold(conn, cursor, d)
+            elif type == "hot":
+                db_insert_hot(conn, cursor, d)
+            elif type == "electricity":
+                db_insert_elect(conn, cursor, d)
+        if type == "cold":
+            res = db_select_cold(conn, cursor)
+        elif type == "hot":
+            res = db_select_hot(conn, cursor)
+        elif type == "electricity":
+            res = db_select_elect(conn, cursor)
+        print_res(res)
+
+
 def main():
     a = ""
     b = ""
-    while a != "e" and a != "E" and a != "s" and a != "S" and a != "m" and a != "M":
-        a = input("Enter, Select or select Month (E/S/M)? ")
+    while a != "e" and a != "E" and a != "s" and a != "S" and a != "m" and a != "M" and a != "export" and a != "import":
+        a = input("Enter, Select, select Month (E/S/M) or export, import (export/import)? ")
     a = a.lower()
-    if a != "m":
+    if a != "m" and a != "export" and a != "import":
         while b != "h" and b != "H" and b != "c" and b != "C" and b != "e" and b != "E":
             b = input("Hot water (H), Cold water (C) or Electricity (E)? ")
         b = b.lower()
@@ -103,7 +142,7 @@ def main():
         else:
             res = db_select_elect(conn, cursor)
         print_res(res)
-    else:
+    elif a == "m":
         res = db_select_hot(conn, cursor)
         if len(res) >= 2:
             val = int(res[-1][1]) - int(res[-2][1])
@@ -126,6 +165,24 @@ def main():
             print("Money for Electricity: ", money)
         else:
             print("Not enough data for Electricity")
+    elif a == "export":
+        data = db_select_hot(conn, cursor)
+        name = "export_hot_water"
+        export_csv(data, name)
+        data = db_select_cold(conn, cursor)
+        name = "export_cold_water"
+        export_csv(data, name)
+        data = db_select_elect(conn, cursor)
+        name = "export_electricity"
+        export_csv(data, name)
+    elif a == "import":
+        type = ""
+        name = ""
+        while type != "cold" and type != "hot" and type != "electricity":
+            type = input("what is type do you want to import (cold, hot, electricity)? ")
+        while name == "" or not os.path.exists(name) or name[-4:] != ".csv":
+            name = input("what is name? ")
+        import_csv(conn, cursor, name, type)
 
     dbclose(conn)
 
